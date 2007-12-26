7a12572dfb8039bef40e32ee48cde46c85f94b95
Internet Systems Consortium <dhcp-announce@isc.org>
2007-12-26 14:00:09 +0100

    - Change the conditions under which a DHCPRELEASE is actually   committed to be consistent with lease binding states rather than   using the lease end time.   This may fix some problems with the   billing class code.
    - Fix a bug where lease updates would fail on Digital Unix (and maybe
      others) because malloc was called with a size of zero.
    
    - Fix a core dump that happens when the DHCP server can't create its
      trace file.

--- server/mdb.c
+++ server/mdb.c
@@ -43,7 +43,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: mdb.c,v 1.60 2001/04/20 20:39:54 mellon Exp $ Copyright (c) 1996-2000 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: mdb.c,v 1.64 2001/04/24 02:31:27 mellon Exp $ Copyright (c) 1996-2000 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -1232,6 +1232,8 @@ int lease_copy (struct lease **lp,
 	if (lease -> uid == lease -> uid_buf) {
 		lt -> uid = lt -> uid_buf;
 		memcpy (lt -> uid_buf, lease -> uid_buf, sizeof lt -> uid_buf);
+	} else if (!lease -> uid_max) {
+		lt -> uid = (unsigned char *)0;
 	} else {
 		lt -> uid = dmalloc (lt -> uid_max, MDL);
 		if (!lt -> uid) {
@@ -1310,7 +1312,11 @@ void release_lease (lease, packet)
 	if (lease -> on_expiry)
 		executable_statement_dereference (&lease -> on_expiry, MDL);
 
-	if (lease -> ends > cur_time) {
+	if (lease -> binding_state != FTS_FREE &&
+	    lease -> binding_state != FTS_BACKUP &&
+	    lease -> binding_state != FTS_RELEASED &&
+	    lease -> binding_state != FTS_EXPIRED &&
+	    lease -> binding_state != FTS_RESET) {
 		if (lease -> on_commit)
 			executable_statement_dereference (&lease -> on_commit,
 							  MDL);
@@ -1328,8 +1334,6 @@ void release_lease (lease, packet)
 #else
 		lease -> next_binding_state = FTS_FREE;
 #endif
-		if (lease -> billing_class)
-			class_dereference (&lease -> billing_class, MDL);
 		supersede_lease (lease, (struct lease *)0, 1, 1, 1);
 	}
 }
@@ -1346,19 +1350,6 @@ void abandon_lease (lease, message)
 	if (!lease_copy (&lt, lease, MDL))
 		return;
 
-#if 0
-	if (lt -> on_expiry)
-		executable_statement_dereference (&lease -> on_expiry, MDL);
-	if (lt -> on_release)
-		executable_statement_dereference (&lease -> on_release, MDL);
-	if (lt -> on_commit)
-		executable_statement_dereference (&lease -> on_commit, MDL);
-
-	/* Blow away any bindings. */
-	if (lt -> scope)
-		binding_scope_dereference (&lt -> scope, MDL);
-#endif
-
 	lt -> ends = cur_time; /* XXX */
 	lt -> next_binding_state = FTS_ABANDONED;
 
@@ -1370,8 +1361,6 @@ void abandon_lease (lease, message)
 	lt -> uid = (unsigned char *)0;
 	lt -> uid_len = 0;
 	lt -> uid_max = 0;
-	if (lt -> billing_class)
-		class_dereference (&lt -> billing_class, MDL);
 	supersede_lease (lease, lt, 1, 1, 1);
 	lease_dereference (&lt, MDL);
 }
@@ -1386,19 +1375,6 @@ void dissociate_lease (lease)
 
 	if (!lease_copy (&lt, lease, MDL))
 		return;
-
-#if 0
-	if (lt -> on_expiry)
-		executable_statement_dereference (&lease -> on_expiry, MDL);
-	if (lt -> on_release)
-		executable_statement_dereference (&lease -> on_release, MDL);
-	if (lt -> on_commit)
-		executable_statement_dereference (&lease -> on_commit, MDL);
-
-	/* Blow away any bindings. */
-	if (lt -> scope)
-		binding_scope_dereference (&lt -> scope, MDL);
-#endif
 
 #if defined (FAILOVER_PROTOCOL)
 	if (lease -> pool && lease -> pool -> failover_peer) {
@@ -1416,8 +1392,6 @@ void dissociate_lease (lease)
 	lt -> uid = (unsigned char *)0;
 	lt -> uid_len = 0;
 	lt -> uid_max = 0;
-	if (lt -> billing_class)
-		class_dereference (&lt -> billing_class, MDL);
 	supersede_lease (lease, lt, 1, 1, 1);
 	lease_dereference (&lt, MDL);
 }
